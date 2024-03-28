0f50bd8f38fcea19c4fd0bfc36a76bd3e5c93a9c
veerendra thakur <veerendrat@proximabiz.com>
2024-03-28 21:07:22 +0530

    Added aut0_commit file

---
+++ auto_commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the number of commits you want to add
+num_commits=51
+
+# Switch to your branch
+git checkout auto_branch
+
+# Loop to create and add commits
+for ((i=1; i<=$num_commits; i++))
+do
+    # Generate a random file content
+    random_content=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+    # Create a new file with random content
+    echo "$random_content" > file_$i.txt
+
+    # Add the new file to the staging area
+    git add file_$i.txt
+
+git add .
+
+    # Commit with a random commit message
+    git commit -m "Commit $i: $random_content"
+done
