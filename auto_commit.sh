0488d6806a0749ce12073645b6f5c32be1897050
veerendra thakur <veerendrat@proximabiz.com>
2024-04-04 14:08:44 +0530

    test Co-auth
    Co-authored-by: vrthakur thakur.viren17@gmail.com

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define the number of commits you want to add
-num_commits=51
+num_commits=101
 
 # Switch to your branch
 git checkout auto_branch
 
 # Loop to create and add commits
-for ((i=1; i<=$num_commits; i++))
+for ((i=52; i<=$num_commits; i++))
 do
     # Generate a random file content
     random_content=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -21,5 +21,6 @@ do
 git add .
 
     # Commit with a random commit message
-    git commit -m "Commit $i: $random_content"
+   # git commit -m "Commit $i: $random_content"
+git commit -m "Commit $i: $random_content" --author "thakurveerendras1 veerendrat+1@proximabiz.com" --signoff
 done
