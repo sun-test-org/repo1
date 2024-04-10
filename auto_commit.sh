784db36d358907eb9552612f152a75471f3ebbe8
veerendra thakur <veerendrat+1@proximabiz.com>
2024-04-10 14:58:22 +0530

    test Co-auth
    Co-authored-by: thakurveerendras1 veerendrat+1@proximabiz.com

---
+++ auto_commit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Define the number of commits you want to add
+num_commits=500
+
+# Switch to your branch
+git checkout auto_branch_500
+
+# Loop to create and add commits
+for ((i=1; i<=$num_commits; i++))
+do
+    # Generate a random file content
+    random_content=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+    # Create a new file with random content
+    echo "$random_content" > file_$i.txt
+
+    # Add the new file to the staging area
+    git add file_$i.txt
+
+ git status
+
+    git add .
+
+ git status
+
+   # Commit with a random commit message
+   # git commit -m "Commit $i: $random_content"
+    git commit -m "test Co-auth" -m "Co-authored-by: thakurveerendras1 veerendrat+1@proximabiz.com"
+done
